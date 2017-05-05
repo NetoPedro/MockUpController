6fb55c262773ac579fe69dba07e1787d3655067d
NetoPedro <pedroneto_09@hotmail.com>
2017-05-05 12:34:36 +0100

    Commit

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -41,10 +41,27 @@ public class ServerThread implements Runnable {
 
             try {
               //  s.setSoTimeout(3000);
-                sIn.read(message);
+                //sIn.read(message);
 
+                int a =0 ;
+                System.out.println("Versio " + (sIn.readUnsignedByte() -48) );
+                System.out.println("SebVersio " + (sIn.readUnsignedByte() -48) );
+                a = sIn.readByte();
+                a+= sIn.readByte()*256;
+                System.out.println("Data_Lenght " + a  );
+                System.out.println("Type " + (sIn.readUnsignedByte() -48) );
+                System.out.println("Encryption " + (sIn.readUnsignedByte() -48) );
+                int timeStamp = 0;
+                timeStamp += sIn.readByte();
+                timeStamp += sIn.readByte()*256;
+                timeStamp += sIn.readByte()*256*256;
+                timeStamp += sIn.readByte()*256*256*256;
+
+                System.out.println("TimeStamp " + timeStamp );
+                System.out.println("Doors number " + (sIn.readUnsignedByte() ));
+                System.out.println("Gate ID " + (sIn.readUnsignedByte()));
                 System.out.println("Mensagem Tipo 1 recebida ");
-                System.out.println(String.valueOf(message));
+               // System.out.println(String.valueOf(message));
 
             } catch(SocketTimeoutException e) {
                 System.out.println("Message reading timeout.");
@@ -80,27 +97,55 @@ public class ServerThread implements Runnable {
         System.out.println("New new status request for " + clientIP.getHostAddress() + ", port number " + s.getPort());
 
         while (true) {
-            /*try {
+            try {
                 Thread.sleep(45000);
             } catch(InterruptedException ex) {
                 Thread.currentThread().interrupt();
             }
-*/
+
             try {
                 sOut = new DataOutputStream(s.getOutputStream());
                 sIn = new DataInputStream(s.getInputStream());
-
                 sOut.write(1);
                 sOut.write(0);
-                sOut.write(ByteBuffer.allocate(2).putShort((short) 0).array());
+                sOut.write(0);
+                sOut.write(0);
+
+                //sOut.write(ByteBuffer.allocate(2).putShort((short) 0).array());
                 sOut.write(2);
                 sOut.write(0);
-                sOut.writeBytes(Timestamp.from(Instant.now()).toString());
+                int tt;
+                int times = Timestamp.from(Instant.now()).getNanos();
+                for (int j = 0; j<4;j++){
+                    tt=times%256;
+                    sOut.write(tt);
+                    times /=256;
+                }
+
                 System.out.println("Mensagem Tipo 2 enviada");
 
                 try {
-                    s.setSoTimeout(5);
-                    sIn.read(message);
+                    s.setSoTimeout(5000);
+                    //sIn.read(message);
+                    int a =0 ;
+                    System.out.println("Versio " + (sIn.readUnsignedByte() -48) );
+                    System.out.println("SebVersio " +(sIn.readUnsignedByte() -48) );
+                    a = sIn.readByte();
+                    a+= sIn.readByte()*256;
+                    System.out.println("Data_Lenght " + a  );
+                    System.out.println("Type " + (sIn.readUnsignedByte() -48) );
+                    System.out.println("Encryption " + (sIn.readUnsignedByte() -48) );
+                    int timeStamp = 0;
+                    timeStamp += sIn.readByte();
+                    timeStamp += sIn.readByte()*256;
+                    timeStamp += sIn.readByte()*256*256;
+                    timeStamp += sIn.readByte()*256*256*256;
+
+                    System.out.println("TimeStamp " + timeStamp );
+                    for(int j = 0; j< a ; j++){
+                        System.out.println("Door " + (sIn.readUnsignedByte()));
+                    }
+                    //System.out.println(message);
                     System.out.println("Mensagem Tipo 3 recebida ");
 
                 } catch (SocketTimeoutException e) {
